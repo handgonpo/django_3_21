508917a6e228aa949f6842efa4f093220757f685
Youjung <handgonpo@naver.com>
2025-03-23 09:40:50 +0900

    [feat] - create - get&post - 403 error

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Topic
+from django.views.decorators.csrf import csrf_exempt
+
 
 def HtmlTemplate(articleTag):
     topics = Topic.objects.all()
@@ -14,9 +16,13 @@ def HtmlTemplate(articleTag):
                 {ol}
             </ul>
             {articleTag}
+            <ul>
+                <li><a href="/create/">Create</a></li>
+            </ul>
         </body>
         </html>
     """
+
 
 def index(request):
     articleTag = """
@@ -25,10 +31,20 @@ def index(request):
     """
     return HttpResponse(HtmlTemplate(articleTag))
 
+
 def read(request, id):
     topic = get_object_or_404(Topic, pk=id)
     article = f"<h2>{topic.title}</h2>{topic.body}"
     return HttpResponse(HtmlTemplate(article))
 
+
+@csrf_exempt
 def create(request):
-    return HttpResponse("Hello, world. Create!")
+    article = """
+    <form action="/create/" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p><textarea name="body" placeholder="body"></textarea></p>
+        <p><input type="submit"></p>
+    </form>
+"""
+    return HttpResponse(HtmlTemplate(article))
